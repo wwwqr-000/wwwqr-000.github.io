f7082dd2153de04dfa55955010d47ac238b74939
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2022-05-02 19:20:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Password Protected page</title>
+<script type="text/javascript">
+var s ="";
+
+while (s!= "marf")
+{
+s=prompt("password needed!");
+if (s=="marf")
+{
+window.location.href="IIIIIIIIIIIIIIIIIIIIIII"; //page to redirect if password entered is correct
+
+}
+else
+{
+alert("Incorrect password-Try again");
+
+}
+}
+</script>
+</head>
+
+<body>
+</body>
+</html>
